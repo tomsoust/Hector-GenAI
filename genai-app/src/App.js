001dfa4701be30c2c73bf928cfb1fa2850a835eb
Tom Soust <tomsoust@gmail.com>
2023-11-09 16:56:36 +1100

    nearly complete

--- genai-app/src/App.js
+++ genai-app/src/App.js
@@ -1,118 +1,97 @@
 import React, { useState } from 'react';
-import { TextField, Button, Select, MenuItem, Typography, Box, createTheme, ThemeProvider, Container, CircularProgress, Drawer } from '@mui/material';
-
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#006400', // darker green color
-    },
-    secondary: {
-      main: '#ffffff', // white color
-    },
-    background: {
-      default: '#333333', // dark grey background color
-    },
-    text: {
-      primary: '#fff', // white text color
-    },
-  },
-});
+import { CircularProgress, Typography, Drawer, List, ListItem, ListItemText } from '@mui/material';
+import './App.css';
 
 function App() {
   const [question, setQuestion] = useState('');
   const [tone, setTone] = useState('');
   const [loading, setLoading] = useState(false);
   const [conversations, setConversations] = useState([]);
+  const clearConversations = (event) => {
+    event.preventDefault();
+    setConversations([]);
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setLoading(true);
-    fetch('/api/hector', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        tone: tone,
-        question: question,
-      }),
-    })
-    .then(response => response.json())
-    .then(data => {
-      setConversations(prevConversations => [...prevConversations, { question: question, answer: '' }]);
 
-      setTimeout(() => {
-        setConversations(prevConversations => {
-          let conversations = [...prevConversations];
-          conversations[conversations.length - 1].answer = data.answer;
-          return conversations;
-        });
-        setQuestion('');
-      }, 1000); // 1000 milliseconds = 1 second
-    })
-    .finally(() => setLoading(false)); // Set loading to false regardless of success or failure
+    try {
+      const response = await fetch('/api/hector', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          tone: tone,
+          question: question,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('API request failed');
+      }
+
+      const data = await response.json();
+
+      setConversations(prevConversations => [...prevConversations, { question: question, answer: data.answer }]);
+      setQuestion('');
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Drawer
-  color="success"
-  invertedColors={false}
-  variant="soft"
->
-  {/* Drawer content */}
-</Drawer>
-      <Container sx={{ backgroundColor: 'black' }}>
-        <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', backgroundColor: 'grey' }}>
-          <Box>
-          <Drawer
-  color="success"
-  invertedColors={false}
-  variant="soft"
->
-  {/* Drawer content */}
-</Drawer>
+    <div className='grid-wrapper'>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', transition: 'background-color 0.5s ease-in-out' }}>
+      <Drawer open={true} variant="permanent" anchor="left">
+        <List>
+          <ListItem>
+            <ListItemText>
+              <Typography variant="h4" style={{ color: '#333333' }}>Hector AI</Typography>
+            </ListItemText>
+          </ListItem>
+        </List>
+      </Drawer>
+      <div style={{ flex: 1, marginLeft: '240px', padding: '20px', backgroundColor: '#e6f7ff', transition: 'margin-left 0.5s ease-in-out' }}>
+        {loading && <CircularProgress />}
+        <div>
           {conversations.map((conversation, index) => (
-            <Box key={index} sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-              <Box sx={{ padding: 1, borderRadius: 1, backgroundColor: 'primary.main', color: 'secondary.main', maxWidth: '80%' }}>
-                <Typography variant="body1" sx={{ color: 'green' }}>You: {conversation.question}</Typography>
-              </Box>
-              <Box sx={{ padding: 1, borderRadius: 1, backgroundColor: 'secondary.main', color: 'primary.main', maxWidth: '80%' }}>
-                <Typography variant="body1" sx={{ color: 'green' }}>Hector: {conversation.answer}</Typography>
-              </Box>
-            </Box>
+            <div key={index} style={{ marginBottom: '10px', transition: 'margin-bottom 0.5s ease-in-out' }}>
+              <div style={{ backgroundColor: '#0066cc', color: '#ffffff', padding: '8px', borderRadius: '4px', maxWidth: '300px', textAlign: 'right', marginLeft: 'auto' }}>
+                <p>You: {conversation.question}</p>
+              </div>
+              <div style={{ backgroundColor: '#e6f7ff', color: '#0066cc', padding: '8px', borderRadius: '4px', maxWidth: '300px', textAlign: 'left', marginRight: 'auto' }}>
+                <p>Hector: {conversation.answer}</p>
+              </div>
+            </div>
           ))}
-          </Box>
-          <Box component="form" onSubmit={handleSubmit} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2 }}>
-            <TextField label="Ask me anything!" variant="outlined" value={question} onChange={e => setQuestion(e.target.value)} sx={{ width: '70%' }} />
-            <Select 
-  value={tone} 
-  onChange={e => setTone(e.target.value)} 
-  variant="outlined" 
-  sx={{ width: '70%' }}
-  MenuProps={{
-    PaperProps: {
-      style: {
-        backgroundColor: '#333333', // dark grey background color
-      },
-    },
-  }}
->
-              <MenuItem value="">Select a tone</MenuItem>
-              <MenuItem value="surfer stoner dude">Surfer Dude</MenuItem>
-              <MenuItem value="sarcastic teenage girl">Sarcastic teenager</MenuItem>
-              <MenuItem value="furious">Angry</MenuItem>
-              <MenuItem value="formal  english">Butler</MenuItem>
-              <MenuItem value="army general">Army General</MenuItem>
-              </Select>
-            <Button type="submit" variant="contained" color="primary">
-              {loading ? <CircularProgress size={24} color="secondary" /> : 'Submit'}
-            </Button>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+        </div>
+        <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <select value={tone} onChange={e => setTone(e.target.value)} style={{ padding: '8px', borderRadius: '4px', marginBottom: '10px', backgroundColor: '#e6f7ff', color: '#0066cc', border: 'none', transition: 'background-color 0.5s ease-in-out' }}>
+            <option value="">Select a tone</option>
+            <option value="surfer stoner dude">Surfer Dude</option>
+            <option value="sarcastic teenage girl">Sarcastic teenager</option>
+            <option value="furious">Angry</option>
+            <option value="formal  english">Butler</option>
+            <option value=" sexy and flirty">Flirty</option>
+            <option value="army general">Army General</option>
+          </select>
+          <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+            <input type="text" placeholder="Ask me anything!" value={question} onChange={e => setQuestion(e.target.value)} style={{ flex: 1, padding: '8px', borderRadius: '4px', border: 'none', transition: 'background-color 0.5s ease-in-out' }} />
+            <button type="submit" style={{ padding: '10px', backgroundColor: '#0066cc', color: '#ffffff', border: 'none', borderRadius: '4px', cursor: 'pointer', transition: 'background-color 0.5s ease-in-out' }}>
+              {loading ? 'Loading...' : 'Submit'}
+            </button>
+          </div>
+          <button onClick={clearConversations} style={{ backgroundColor: '#0066cc', color: '#ffffff', padding: '8px', border: 'none', borderRadius: '4px', cursor: 'pointer', transition: 'background-color 0.5s ease-in-out' }}>
+            Clear Conversation
+          </button>
+        </form>
+      </div>
+    </div>
+    </div>
   );
 }
 
